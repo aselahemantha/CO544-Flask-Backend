290a28342a6a6ce8b32bba99a36997601f3e39dd
L.M.A.H. Premawansha <78247019+aselahemantha@users.noreply.github.com>
2024-06-24 09:45:53 +0530

    Update app.py for model fitting

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+
+model_path = './models/random_forest.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return "Welcome to the Random Forest Classifier API!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json(force=True)
+
+        data_df = pd.DataFrame([data])
+
+        prediction = model.predict(data_df)
+
+        return jsonify({'Diabetic Prediction': prediction.tolist()})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8888)
